6db6b610919ebd90879b0c9330b8a742f9ae1255
MechTheLegend <147281376+MechTheLegend@users.noreply.github.com>
2024-01-22 21:49:42 -0500

    added mild adjustments and updated about me aswell aswell as using chatgpt to get basic clicker game up and running

---
+++ games/clicker game/script.js
@@ -0,0 +1,9 @@
+let score = 0;
+
+function handleClick() {
+  score++;
+
+  document.getElementById('scoreValue').textContent = score;
+}
+
+document.getElementById('clickerButton').addEventListener('click', handleClick);
