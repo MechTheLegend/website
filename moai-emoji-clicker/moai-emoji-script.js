92c49689adde23cd5c7fd78178b35424298d1ae6
MechTheLegend <147281376+MechTheLegend@users.noreply.github.com>
2024-02-10 13:27:04 -0500

    updated clicker game (it finally works now :DDDDD)

--- moai-emoji-clicker/moai-emoji-script.js
+++ moai-emoji-clicker/moai-emoji-script.js
@@ -1,11 +1,34 @@
 let score = 0;
+let clickRate = 1;
+let texterCost = 50;
 
 function handleClick() {
-  score++;
+  score=Math.floor(score+=clickRate);
 
+  updateScoreDisplay();
+}
+
+function handleTexterPurchase() {
+  if (score >= texterCost) {
+    score=Math.floor(score -= texterCost);
+
+    clickRate++;
+
+    texterCost=Math.floor(texterCost *= 1.5);
+
+    updateScoreDisplay();
+    updateTexterCost();
+  }
+}
+
+function updateScoreDisplay() {
   document.getElementById('scoreValue').textContent = score;
 }
 
-document.getElementById('clickerButton').addEventListener('click', handleClick);
+function updateTexterCost() {
+  document.getElementById('texterCost').textContent = texterCost;
+}
 
+document.getElementById('clickButton').addEventListener('click', handleClick);
 
+document.getElementById('buyTexter').addEventListener('click', handleTexterPurchase);
