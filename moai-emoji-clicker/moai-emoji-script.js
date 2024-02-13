4f3798864c652d551d19f8dd572ee0d952370d6e
MechTheLegend <147281376+MechTheLegend@users.noreply.github.com>
2024-02-12 21:33:13 -0500

    Added 2 more upgraders

--- moai-emoji-clicker/moai-emoji-script.js
+++ moai-emoji-clicker/moai-emoji-script.js
@@ -1,6 +1,8 @@
 let score = 0;
 let clickRate = 1;
 let texterCost = 50;
+let redditUserCost= 100;
+let memePosterCost= 500;
 
 function handleClick() {
   score=Math.floor(score+=clickRate);
@@ -12,12 +14,38 @@ function handleTexterPurchase() {
   if (score >= texterCost) {
     score=Math.floor(score -= texterCost);
 
-    clickRate++;
+    clickRate=clickRate+1;
 
     texterCost=Math.floor(texterCost *= 1.5);
 
     updateScoreDisplay();
     updateTexterCost();
+  }
+}
+
+  function handleRedditUserPurchase() {
+    if (score >= redditUserCost) {
+      score=Math.floor(score -= redditUserCost);
+  
+      clickRate=clickRate+2;
+  
+      redditUserCost=Math.floor(redditUserCost *= 1.5);
+  
+      updateScoreDisplay();
+      updateRedditUserCost();
+    }
+}
+
+function handleMemePosterPurchase() {
+  if (score >= memePosterCost) {
+    score=Math.floor(score -= memePosterCost);
+
+    clickRate=clickRate+5;
+
+    memePosterCost=Math.floor(memePosterCost *= 1.5);
+
+    updateScoreDisplay();
+    updateMemePosterCost();
   }
 }
 
@@ -29,6 +57,18 @@ function updateTexterCost() {
   document.getElementById('texterCost').textContent = texterCost;
 }
 
+function updateRedditUserCost() {
+  document.getElementById('redditUserCost').textContent = redditUserCost;
+}
+
+function updateMemePosterCost() {
+  document.getElementById('memePosterCost').textContent = memePosterCost;
+}
+
 document.getElementById('clickButton').addEventListener('click', handleClick);
 
 document.getElementById('buyTexter').addEventListener('click', handleTexterPurchase);
+
+document.getElementById('buyRedditUser').addEventListener('click', handleRedditUserPurchase)
+
+document.getElementById('buyMemePoster').addEventListener('click', handleMemePosterPurchase)
